e7a30d14aae6290dbac302d9d31084dd7e895b46
hyewon_park2 <hyewon_park2@tmax.co.kr>
2024-04-04 10:00:35 +0900

    error boundary 공부 및 Image 태그

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Counter from "@/components/Counter";
+import Image from "next/image";
 import os from "os"; // 노드 API
 export default function Home() {
   console.log("안녕-서버");
@@ -8,6 +9,12 @@ export default function Home() {
     <div>
       <h1>홈페이지다 !</h1>
       <Counter />
+      <Image
+        alt="shop"
+        width={400}
+        height={400}
+        src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
+      />
     </div>
   );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,18 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
-export const revalidate = 3;
+import Image from "next/image";
+// export const revalidate = 3;
+import clotehsImage from "../../../public/images/clothes.jpg";
+
+import MeowArticle from "@/components/MeowArticle";
 export default async function ProductsPage() {
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
   const products = await getProducts();
+
   return (
     <>
       <h1>제품 소개 페이지!</h1>
+      <Image src={clotehsImage} alt="Clotehs" />
       <ul>
         {products.map((product, index) => (
           <Link href={`/products/${product.id}`} key={index}>
@@ -14,6 +20,7 @@ export default async function ProductsPage() {
           </Link>
         ))}
       </ul>
+      {/* <MeowArticle /> */}
     </>
   );
 }
