9ff434991e0f746437fe650497f7f835c041bd71
heywon0909 <hw7319@naver.com>
2024-04-08 22:15:53 +0900

    feat: loading UI 추가

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,14 @@
 import { getProducts } from "@/service/products";
 import Link from "next/link";
-export const revalidate = 3;
+// export const revalidate = 3;
+// import styles from './page.module.css'
+import MeowArticle from '@/components/MeowArticle';
 export default async function ProductsPage() {
+  
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
   const products = await getProducts();
+
+
   return (
     <>
       <h1>제품 소개 페이지!</h1>
@@ -14,6 +19,7 @@ export default async function ProductsPage() {
           </Link>
         ))}
       </ul>
+     <MeowArticle/>
     </>
   );
 }
--- src/service/products.ts
+++ src/service/products.ts
@@ -7,6 +7,7 @@ export type Product = {
   price: number;
 };
 export async function getProducts(): Promise<Product[]> {
+  for (let i = 0; i < 1000000000; i++) {}
   const filePath = path.join(process.cwd(), "data", "products.json");
 
   const data = await fs.readFile(filePath, "utf-8");
