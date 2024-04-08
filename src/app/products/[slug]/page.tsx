c90dbd32921efe3fffecfec08e722271fd9bbfa8
hyewon_park2 <hyewon_park2@tmax.co.kr>
2024-04-08 09:46:18 +0900

    feat: variable font 추가

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getProduct, getProducts } from "@/service/products";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 type Props = {
@@ -17,7 +18,17 @@ export default async function PantsPage({ params: { slug } }: Props) {
   if (!product) {
     notFound();
   }
-  return <h1>{product.name} 제품 설명 페이지</h1>;
+  return (
+    <>
+      <h1> {product.name} 제품 설명 페이지</h1>
+      <Image
+        src={`/images/${product.image}`}
+        alt={product.name}
+        width="300"
+        height="300"
+      />
+    </>
+  );
 }
 
 export async function generateStaticParams() {
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 // export const revalidate = 3;
 import clotehsImage from "../../../public/images/clothes.jpg";
 
-import MeowArticle from "@/components/MeowArticle";
 export default async function ProductsPage() {
   // 서버 파일(데이터베이스)에 있는 제품의 리스트를 읽어와서, 그걸 보여줌
   const products = await getProducts();
@@ -15,9 +14,11 @@ export default async function ProductsPage() {
       <Image src={clotehsImage} alt="Clotehs" />
       <ul>
         {products.map((product, index) => (
-          <Link href={`/products/${product.id}`} key={index}>
-            <li key={index}>{product.name}</li>
-          </Link>
+          <>
+            <Link href={`/products/${product.id}`} key={index}>
+              <li key={index}>{product.name}</li>
+            </Link>
+          </>
         ))}
       </ul>
       {/* <MeowArticle /> */}
